81d54b98d65d056bd29f00e084ac53eb8618dec6
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2020-08-21 11:24:04 +0200

    Reorganised inputs

--- test_dir_utilities.sh
+++ test_dir_utilities.sh
@@ -2,8 +2,14 @@
 
 # utilities tests
 utilities_DIRS=(\
-	"utilities/tree_isomorphism" "utilities/tree_isomorphism" \
+	"utilities/tree_isomorphism/free/positive" "utilities/tree_isomorphism/free/positive" \
+	"utilities/tree_isomorphism/free/negative" "utilities/tree_isomorphism/free/negative" \
+	"utilities/tree_isomorphism/rooted/positive" "utilities/tree_isomorphism/rooted/positive" \
+	"utilities/tree_isomorphism/rooted/negative" "utilities/tree_isomorphism/rooted/negative" \
 	)
 utilities_isomorphism_DIRS=(\
-	"utilities/tree_isomorphism" "utilities/tree_isomorphism" \
+	"utilities/tree_isomorphism/free/positive" "utilities/tree_isomorphism/free/positive" \
+	"utilities/tree_isomorphism/free/negative" "utilities/tree_isomorphism/free/negative" \
+	"utilities/tree_isomorphism/rooted/positive" "utilities/tree_isomorphism/rooted/positive" \
+	"utilities/tree_isomorphism/rooted/negative" "utilities/tree_isomorphism/rooted/negative" \
 	)
