d69b09ab6ae2ee963198c8a40c61b80ecd11dda1
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2020-11-06 11:18:46 +0100

    Added missing directory

--- test_dir_utilities.sh
+++ test_dir_utilities.sh
@@ -6,10 +6,12 @@ utilities_DIRS=(\
 	"utilities/tree_isomorphism/free/negative" "utilities/tree_isomorphism/free/negative" \
 	"utilities/tree_isomorphism/rooted/positive" "utilities/tree_isomorphism/rooted/positive" \
 	"utilities/tree_isomorphism/rooted/negative" "utilities/tree_isomorphism/rooted/negative" \
+	"utilities/tree_isomorphism/" "utilities/tree_isomorphism" \
 	)
 utilities_isomorphism_DIRS=(\
 	"utilities/tree_isomorphism/free/positive" "utilities/tree_isomorphism/free/positive" \
 	"utilities/tree_isomorphism/free/negative" "utilities/tree_isomorphism/free/negative" \
 	"utilities/tree_isomorphism/rooted/positive" "utilities/tree_isomorphism/rooted/positive" \
 	"utilities/tree_isomorphism/rooted/negative" "utilities/tree_isomorphism/rooted/negative" \
+	"utilities/tree_isomorphism/" "utilities/tree_isomorphism" \
 	)
