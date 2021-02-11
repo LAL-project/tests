15af2a73010cc69b38fde799f216e821b6f36254
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2021-02-11 17:25:13 +0100

    Updated submission script

--- submit_tests.sh
+++ submit_tests.sh
@@ -2,36 +2,56 @@
 
 mode=$1 # release, debug
 info=info_$mode
+
+valgrind_param=""
+use_valgrind=0
+if [ ! -z $2 ]; then
+	if [ "$mode" == "debug" ] && [ "$2" == "valgrind" ]; then
+		use_valgrind=1
+		valgrind_param="--valgrind"
+		info="$info"_valgrind
+	fi
+fi
+
+queue="short"
+if [ "$mode" == "debug" ]; then
+	if [ $use_valgrind -eq 1 ]; then
+		queue="long"
+	else
+		queue="medium"
+	fi
+fi
+
 mkdir -p $info
 
 group=generate
 echo $group" - "$mode
-sbatch --job-name=test_"$mode" -p short -o $info/"$group".out -e $info/"$group".err ./test.sh --$mode --exe-group=$group --log-file=$info/"$group"_"$mode"_log
+sbatch --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_"$mode"_log --storage-dir=$info
 
 group=graphs
 echo $group" - "$mode
-sbatch --job-name=test_"$mode" -p short -o $info/"$group".out -e $info/"$group".err ./test.sh --$mode --exe-group=$group --log-file=$info/"$group"_"$mode"_log
+sbatch --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_"$mode"_log --storage-dir=$info
 
 group=internal
 echo $group" - "$mode
-sbatch --job-name=test_"$mode" -p short -o $info/"$group".out -e $info/"$group".err ./test.sh --$mode --exe-group=$group --log-file=$info/"$group"_"$mode"_log
+sbatch --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_"$mode"_log --storage-dir=$info
 
 group=linarr
 echo $group" - "$mode
-sbatch --job-name=test_"$mode" -p short -o $info/"$group".out -e $info/"$group".err ./test.sh --$mode --exe-group=$group --log-file=$info/"$group"_"$mode"_log
+sbatch --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_"$mode"_log --storage-dir=$info
 
 group=memory
 echo $group" - "$mode
-sbatch --job-name=test_"$mode" -p short -o $info/"$group".out -e $info/"$group".err ./test.sh --$mode --exe-group=$group --log-file=$info/"$group"_"$mode"_log
+sbatch --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_"$mode"_log --storage-dir=$info
 
 group=numeric
 echo $group" - "$mode
-sbatch --job-name=test_"$mode" -p short -o $info/"$group".out -e $info/"$group".err ./test.sh --$mode --exe-group=$group --log-file=$info/"$group"_"$mode"_log
+sbatch --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_"$mode"_log --storage-dir=$info
 
 group=properties
 echo $group" - "$mode
-sbatch --job-name=test_"$mode" -p short -o $info/"$group".out -e $info/"$group".err ./test.sh --$mode --exe-group=$group --log-file=$info/"$group"_"$mode"_log
+sbatch --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_"$mode"_log --storage-dir=$info
 
 group=utilities
 echo $group" - "$mode
-sbatch --job-name=test_"$mode" -p short -o $info/"$group".out -e $info/"$group".err ./test.sh --$mode --exe-group=$group --log-file=$info/"$group"_"$mode"_log
+sbatch --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_"$mode"_log --storage-dir=$info
