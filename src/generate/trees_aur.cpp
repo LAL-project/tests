4963812329ca9fa69eb9a6bbb086ea6cc453a823
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2021-02-22 10:37:05 +0100

    Updated year in LICENSE header

--- src/generate/trees_aur.cpp
+++ src/generate/trees_aur.cpp
@@ -3,7 +3,7 @@
  *  Tests of the Linear Arrangement Library - Programs used to test the
  *  algorithms in the linear arrangement library.
  *
- *  Copyright (C) 2019-2020
+ *  Copyright (C) 2019 - 2021
  *
  *  This file is part of Tests of the Linear Arrangement Library.
  *
