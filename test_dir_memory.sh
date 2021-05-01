4b9e7084485e96e59fa4c8ebfd811ec2f4cef2c7
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2021-05-01 09:52:47 +0200

    Added missing variables

--- test_dir_memory.sh
+++ test_dir_memory.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 # numeric tests
-memory_DIRS=(\
+memory_numeric_DIRS=(\
 	"memory/numeric" "memory/numeric" \
+	)
+
+# graphs tests
+memory_graphs_DIRS=(\
 	"memory/graphs" "memory/graphs" \
+	)
+
+# internal tests
+memory_internal_DIRS=(\
 	"memory/internal" "memory/internal" \
+	)
+
+# memory tests
+memory_DIRS=(\
+	"${memory_numeric_DIRS[@]}" \
+	"${memory_graphs_DIRS[@]}" \
+	"${memory_internal_DIRS[@]}" \
 	)
