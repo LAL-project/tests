bb871b140ca171a7d415035c8dc6277bfe585e68
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2022-03-19 16:29:07 +0100

    Use 8 threads to compile

--- submit_compilation.sh
+++ submit_compilation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./make_makefiles_cluster.sh
-sbatch -p short -c 4 compile_cluster.sh build-release
-sbatch -p short -c 4 compile_cluster.sh build-debug
+sbatch -p short -c 8 compile_cluster.sh build-release
+sbatch -p short -c 8 compile_cluster.sh build-debug
