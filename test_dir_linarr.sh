55f36885c30529c1b27f14dc2032e07e4bda5b2b
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2022-11-30 11:48:25 +0100

    Removed one unimplemented test group

--- test_dir_linarr.sh
+++ test_dir_linarr.sh
@@ -184,13 +184,9 @@ linarr_DMax_Planar_AEF_DIRS=(\
 linarr_DMax_Planar_DIRS=(\
 	"${linarr_DMax_Planar_AEF_DIRS[@]}" \
 	)
-linarr_DMax_Comparison_DIRS=(\
-	"linarr/D/DMax/" "linarr/D/DMax/" \
-	)
 linarr_DMax_DIRS=(\
 	"${linarr_DMax_Projective_DIRS[@]}" \
 	"${linarr_DMax_Planar_DIRS[@]}" \
-	"${linarr_DMax_Comparison_DIRS[@]}" \
 	)
 
 # Classification of syntactic dependency structures
