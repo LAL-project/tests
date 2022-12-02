d688c1b484acd899db257f2612aa42997f6e1494
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2022-12-02 10:09:56 +0100

    Updated submission script

--- submit_tests.sh
+++ submit_tests.sh
@@ -1,58 +1,90 @@
 #!/bin/bash
 
-mode=$1 # release, debug
-if [ "$mode" != "release" ] && [ "$mode" != "debug" ]; then
-	echo "Wrong mode of execution: $mode"
-	echo "    ./submit_tests.sh release/debug"
+# build type (debug, release)
+build=0
+# analyzer type (valgrind, address-sanitizer
+
+for i in "$@"; do
+	case $i in
+		
+		--help|-h)
+		usage=1
+		shift
+		;;
+		
+		--build=*)
+		build="${i#*=}"
+		shift
+		;;
+		
+		--analyzer=*)
+		analyzer="${i#*=}"
+		shift
+		;;
+		
+		*)
+		echo -e "\e[1;4;31mError:\e[0m Option $i unknown"
+		exit
+		;;
+	esac
+done
+
+if [ "$build" != "debug" ] && [ "$build" != "release" ]; then
+	echo -e "\e[1;4;31mError:\e[0m Wrong value for build: '$build'"
+	echo "    Allowed values are: 'debug', 'release'."
 	exit
 fi
 
-info=info_$mode
+if [ ! -z $analyzer ]; then
+	
+	if [ "$analyzer" != "valgrind" ] && [ "$analyzer" != "address-sanitizer" ]; then
+		echo -e "\e[1;4;31mError:\e[0m Wrong value for analyzer: '$analyzer'"
+		echo "    Allowed values are: 'valgrind', 'address-sanitizer'."
+		exit
+	fi
+	
+fi
+
+info=info_$build
 valgrind_param=""
 use_valgrind=0
-memory=2GB
+memory=4GB
 
-if [ ! -z $2 ]; then
-	if [ "$mode" == "debug" ] && [ "$2" == "valgrind" ]; then
+if [ ! -z $analyzer ]; then
+	
+	if [ "$analyzer" == "valgrind" ]; then
+		# valgrind analyzer can only be used in debug compilations
+		if [ "$build" != "debug" ]; then
+			echo -e "\e[1;4;31mError:\e[0m Cannot use valgrind in a non-debug build"
+			exit
+		fi
+		
 		use_valgrind=1
 		valgrind_param="--valgrind"
 		info="$info"_valgrind
-		memory=4GB
+		memory=8GB
+		
+	elif [ "$analyzer" == "address-sanitizer" ]; then
+		# address sanitizer can be used in both debug and release compilations
+		memory=8GB
+		
 	fi
+	
 fi
 
 queue="long"
 
+echo "Execution information"
+echo "    build: '$build'"
+echo "    analyzer: '$analyzer'"
+echo "    job memory: '$memory'"
+echo "    job queue: '$queue'"
+echo "    use valgrind: '$use_valgrind'"
+echo "        valgrind param: '$valgrind_param'"
+
 mkdir -p $info
 
-group=generate
-echo $group" - "$mode
-sbatch --mem=$memory --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_log --storage-dir=$info
-
-group=graphs
-echo $group" - "$mode
-sbatch --mem=$memory --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_log --storage-dir=$info
-
-group=detail
-echo $group" - "$mode
-sbatch --mem=$memory --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_log --storage-dir=$info
-
-group=linarr
-echo $group" - "$mode
-sbatch --mem=$memory --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_log --storage-dir=$info
-
-group=memory
-echo $group" - "$mode
-sbatch --mem=$memory --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_log --storage-dir=$info
-
-group=numeric
-echo $group" - "$mode
-sbatch --mem=$memory --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_log --storage-dir=$info
-
-group=properties
-echo $group" - "$mode
-sbatch --mem=$memory --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_log --storage-dir=$info
-
-group=utilities
-echo $group" - "$mode
-sbatch --mem=$memory --job-name=tests_"$mode" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$mode --exe-group=$group --log-file="$group"_log --storage-dir=$info
+for group in detail generate graphs linarr memory numeric properties utilities; do
+	echo $group" - "$build
+	sbatch --mem=$memory --job-name=tests_"$build" -p $queue -o $info/"$group".out -e $info/"$group".err ./test.sh $valgrind_param --$build --exe-group=$group --log-file="$group"_log --storage-dir=$info
+done
