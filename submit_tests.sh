87ee8f3efd6f720f2a14eac790a7b39f740710b5
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2023-06-05 10:22:32 +0200

    Complete submission of tests to the cluster

--- submit_tests.sh
+++ submit_tests.sh
@@ -8,8 +8,8 @@ function show_usage {
 	echo "    --mode=debug,release"
 	echo "        What to run: a debug build, or a release build"
 	echo ""
-	echo "    --exe-directory=dir"
-	echo "        the directory that contains the executable file"
+	echo "    --compiler-version=dir"
+	echo "        This is used to make get the name of the directory that contains the executable file"
 	echo ""
 	echo "Optional parameters:"
 	echo "    --analyzer=valgrind,address-sanitizer"
@@ -39,9 +39,9 @@ for i in "$@"; do
 		build="${i#*=}"
 		shift
 		;;
-		
-		--exe-directory=*)
-		exe_directory="${i#*=}"
+
+		--compiler-version=*)
+		compiler_version="${i#*=}"
 		shift
 		;;
 		
@@ -67,8 +67,8 @@ if [ -z $build ]; then
 	exit
 fi
 
-if [ -z $exe_directory ]; then
-	echo -e "\e[1;4;31mError:\e[0m Execution directory parameter cannot be empty"
+if [ -z $compiler_version ]; then
+	echo -e "\e[1;4;31mError:\e[0m Missing compiler version"
 	exit
 fi
 
@@ -88,7 +88,9 @@ if [ ! -z $analyzer ]; then
 	
 fi
 
-info=info_$exe_directory
+exe_directory=build-$build-$compiler_version
+log_directory=info_$exe_directory
+
 valgrind_param=""
 use_valgrind=0
 memory=4GB
@@ -115,15 +117,21 @@ queue="long"
 echo "Execution information"
 echo "    build: '$build'"
 echo "    exe_directory: '$exe_directory'"
+echo "    log_directory: '$log_directory'"
 echo "    analyzer: '$analyzer'"
 echo "    job memory: '$memory'"
 echo "    job queue: '$queue'"
 echo "    use valgrind: '$use_valgrind'"
 echo "        valgrind param: '$valgrind_param'"
 
-mkdir -p $info
+mkdir -p $log_directory
 
 for group in detail generate graphs linarr memory numeric properties utilities; do
+	job_name=tests_"$build"_"$group"_"$compiler_version"
+
 	echo $group" - "$build
-	echo "sbatch --mem=$memory --job-name=tests_$build -p $queue -o $info/job__$group.out -e $info/job__$group.err ./test.sh $valgrind_param --$build --exe-directory=$exe_directory --exe-group=$group --log-file=log_$group --storage-dir=$info"
+	echo "    job_name: $job_name"
+
+	sbatch --mem=$memory --job-name=$job_name -p $queue -o $log_directory/job__$group.out -e $log_directory/job__$group.err ./test.sh $valgrind_param --$build --exe-directory=$exe_directory --exe-group=$group --log-file=log_$group --storage-dir=$log_directory
 done
+
