e5db61fe7caf829767c071634caa47c8e0b0acda
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-07-18 13:30:55 +0200

    Allow generic location for nauty

--- make_makefiles_cluster.sh
+++ make_makefiles_cluster.sh
@@ -17,14 +17,17 @@ function make_directories_compiler() {
 		C_compiler="-DCMAKE_C_COMPILER=/home/soft/gcc-11.4.0/bin/gcc"
 	fi
 	
+	LAL="-DLAL_DEV_DIR=$HOME/LAL-dev/latest/linear-arrangement-library"
+	NAUTY="-DNAUTY_DIR=$HOME/installations/nauty/2_8_8"
+	
 	mkdir -p build-debug$suffix
 	cd build-debug$suffix
-	$CMAKE_DIR/cmake $CXX_compiler $C_compiler -DCMAKE_BUILD_TYPE=Debug -DLAL_DEV_DIR=$HOME/LAL-dev/latest/linear-arrangement-library ../src
+	$CMAKE_DIR/cmake $CXX_compiler $C_compiler -DCMAKE_BUILD_TYPE=Debug $LAL $NAUTY ../src
 	cd ..
 
 	mkdir -p build-release$suffix
 	cd build-release$suffix
-	$CMAKE_DIR/cmake $CXX_compiler $C_compiler -DCMAKE_BUILD_TYPE=Release -DLAL_DEV_DIR=$HOME/LAL-dev/latest/linear-arrangement-library ../src
+	$CMAKE_DIR/cmake $CXX_compiler $C_compiler -DCMAKE_BUILD_TYPE=Release $LAL $NAUTY ../src
 	cd ..
 }
 
