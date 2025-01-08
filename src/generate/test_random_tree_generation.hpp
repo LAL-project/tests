b177efd3e9003ba449c7225b2ec5356cb97f447e
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2025-01-08 11:23:56 +0100

    Updated copyright notice

--- src/generate/test_random_tree_generation.hpp
+++ src/generate/test_random_tree_generation.hpp
@@ -3,7 +3,7 @@
  * Tests of the Linear Arrangement Library - Programs used to test the
  * algorithms in the linear arrangement library.
  *
- * Copyright (C) 2019 - 2024
+ * Copyright (C) 2019 - 2025
  *
  * This file is part of Tests of the Linear Arrangement Library. The full code
  * is available at:
